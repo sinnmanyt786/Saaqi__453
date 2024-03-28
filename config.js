c16dfaabc3cc4133af1d84b4044022ec79bb693b
sinnmanyt786 <160015894+sinnmanyt786@users.noreply.github.com>
2024-03-29 01:25:34 +0500

    Update config.js

--- config.js
+++ config.js
@@ -15,10 +15,10 @@ global.devs = '923407472645';
 global.website = 'https://github.com/chhaseeb47/HASI-MD' // Hello world
 global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://telegra.ph/file/ea5cfd778d2004c72d965.jpg'
 module.exports = {
-  botname:   process.env.BOT_NAME === undefined ? 'HASI-MD' : process.env.BOT_NAME,
-  ownername: process.env.OWNER_NAME === undefined ? '𝗖𝗛 𝗛𝗔𝗦𝗘𝗘𝗕' : process.env.OWNER_NAME,
-  sessionName:  process.env.SESSION_ID === undefined ? false : process.env.SESSION_ID,
-  author:  process.env.PACK_AUTHER.split(";")[0] === undefined ? '𝗖𝗛 𝗛𝗔𝗦𝗘𝗘𝗕' : process.env.PACK_AUTHER.split(";")[0],
+  botname:   process.env.BOT_NAME === Saaqi__453? 'Saaqi__453' : process.env.BOT_NAME,Saaqi__453
+  ownername: process.env.OWNER_NAME === undefined ? ' Saaqi__453. : process.env.OWNER_NAME,Saaqi__453
+  sessionName:  process.env.SESSION_ID === Saaqi__453? false : process.env.SESSION_ID,
+  author:  process.env.PACK_AUTHER.split(";")[0] === ? 'Saaqi__453 : process.env.PACK_AUTHER.split(";")[0],
   auto_read_status :  process.env.AUTO_READ_STATUS === undefined ? false : process.env.AUTO_READ_STATUS,
   packname:  process.env.PACK_NAME.split(";")[1] === undefined ? 'HASI-MD' : process.env.PACK_NAME.split(";")[1],
   autoreaction:  process.env.AUTO_REACTION  === undefined ? false : process.env.AUTO_REACTION ,
